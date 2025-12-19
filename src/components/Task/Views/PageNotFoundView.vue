bf8398467c106df5c4621c6a83c6c8e5d1139111
Anastasija <valkyrie22fi@MacBook-Air-Anastasia.local>
2025-12-19 20:26:49 +0300

    Task 9.6

---
+++ src/components/Task/Views/PageNotFoundView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <p>Страница не найдена</p>
+    <router-link to="/">Вернуться на главную</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageNotFoundView"
+};
+</script>
+
+<style scoped>
+.not-found {
+  padding: 20px;
+  text-align: center;
+}
+
+.not-found h1 {
+  font-size: 48px;
+  margin-bottom: 10px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/components/Task/Views/HomeView.vue";
 import MyProdactsView from "@/components/Task/Views/MyProdactsView.vue";
+import PageNotFoundView from "@/components/Task/Views/PageNotFoundView.vue";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: "/products",
     name: "MyProdacts",
     component: MyProdactsView
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: PageNotFoundView
   }
 ];
 
